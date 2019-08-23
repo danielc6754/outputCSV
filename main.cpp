b80b6d121e095cd9c1ec3b1c6361746ae926b30e
danielc6754 <daniel.c6754@gmail.com>
2019-08-23 16:51:01 -0700

    structured and organized code

--- main.cpp
+++ main.cpp
@@ -12,6 +12,32 @@ bool fileExist(const std::string& filename) {
 	return stat(filename.c_str(), &buf) == 0;
 }
 
+void dmgConStrCalc(int *arrConStr) {
+
+	for (int i = 0; i < 25; i++) {
+		for (int j = 0; j < 25; j++) {
+			int hp = 50;
+			int hitcount = 1;
+
+			// Set con and str
+			int con = (j + 1);
+			int str = ((i + 1) * 3 + 4);
+
+			int dmg = con - str;
+			if (dmg > 0)
+				dmg = 0;
+
+			arrConStr[j * 25 + i] = 15 + dmg;
+		}
+	}
+
+	return;
+}
+
+int dodgeCritCalc(int luk) {
+	return 0;
+}
+
 int main() {
 	
 	string name = "damageCalc.csv";
@@ -19,33 +45,36 @@ int main() {
 	// Only run data function if file exists
 	if (fileExist(name)) {
 
-		// Create some data points
+		// Create holding data
+		int arrConStr[625];
 
-
+		// Generate data
+		dmgConStrCalc(arrConStr);
+		
+		// Output data
 		ofstream myfile;
 		myfile.open(name);
 
-		myfile << "STR, CON, ";
+		// Constitution and Strength Relation
+		myfile << "CON \\ STR";
+		for (int i = 1; i < 26; i++)
+			myfile << "," << i;
 
-		// Generate data here
-		for (int i = 1; i < 101; i++) {
-			myfile << i << ",";
-		}
-
+		int conValue = 1;
 		myfile << "\n";
 
-		for (int con = 0; con < 101; con++) {
-			myfile << "," << con << ",";
-			for (int str = 1; str < 101; str++) {
-				int dmg = con - str;
-				if (dmg > 0)
-					dmg = 0;
-
-				myfile << dmg << ",";
+		for (int i = 0; i < 625; i++) {
+			if (i % 25 == 0) {
+				myfile << conValue;
+				conValue++;
 			}
 
-			myfile << "\n";
+			myfile << "," << arrConStr[i];
+
+			if ((i + 1) % 25 == 0)
+				myfile << "\n";
 		}
+
 
 		myfile.close();
 	}
